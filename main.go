45510648dccb84627c29974bb0b77f79edb08ae7
jigkoxsee <jigko@jigko.me>
2015-08-29 23:56:37 +0700

    It's work but need some test

--- main.go
+++ main.go
@@ -20,12 +20,106 @@ type Step struct {
 }
 type State struct {
 	board [][]int
-	bank  []int
+	blank []int
 	sol   string
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world!")
+}
+
+// BFS Call this
+func bfs(goal, init [][]int, blank []int) State { // return []Step
+	var stateQ []State = make([]State, 1)
+	stateQ[0].board = init
+	stateQ[0].sol = ""
+	stateQ[0].blank = blank
+	fmt.Println(stateQ)
+	i := 0
+	//for {
+	for q := 0; ; q++ {
+		// Dequeue, check with goal
+		if checkIdentical(stateQ[q].board, goal) {
+			fmt.Println("---------------SUCCESS------------------")
+			fmt.Println(stateQ[q])
+			print(stateQ[q].board)
+			return stateQ[q]
+		}
+		// Move UDLR , Enqueue
+		if u, err := bfsMove(&stateQ[q], "U"); err == nil {
+			i++
+			//stateQ[i] = u
+			stateQ = append(stateQ, u)
+		}
+		if d, err := bfsMove(&stateQ[q], "D"); err == nil {
+			i++
+			//stateQ[i] = d
+			stateQ = append(stateQ, d)
+		}
+		if l, err := bfsMove(&stateQ[q], "L"); err == nil {
+			i++
+			//stateQ[i] = l
+			stateQ = append(stateQ, l)
+		}
+		if r, err := bfsMove(&stateQ[q], "R"); err == nil {
+			i++
+			//stateQ[i] = r
+			stateQ = append(stateQ, r)
+		}
+	}
+	fmt.Println(stateQ)
+	return State{} // For test only must change!!!
+}
+
+func bfsMove(s *State, dir string) (State, error) {
+	fmt.Println("--------BFS-Gen :", dir)
+	print(s.board)
+	// Check what is last move and not to counter it
+	// ie. if last what U then not D (return err)
+	if s.sol[len(s.sol):] == "U" && dir == "D" {
+		fmt.Println("No counter move")
+		return State{}, errors.New("No counter")
+	}
+	if s.sol[len(s.sol):] == "D" && dir == "U" {
+		fmt.Println("No counter move")
+		return State{}, errors.New("No counter")
+	}
+	if s.sol[len(s.sol):] == "L" && dir == "R" {
+		fmt.Println("No counter move")
+		return State{}, errors.New("No counter")
+	}
+	if s.sol[len(s.sol):] == "R" && dir == "L" {
+		fmt.Println("No counter move")
+		return State{}, errors.New("No counter")
+	}
+	// Next move
+	board := copyBoard(s.board)
+	blank := copyBlank(s.blank)
+	board, blank, err := move(board, blank, dir)
+	if err != nil {
+		fmt.Println("Can't move!!!!!")
+		return State{}, errors.New("Can't move")
+	}
+	sol := s.sol + dir
+	// Check is this state was past before. if is was return err
+	return State{board: board, blank: blank, sol: sol}, err
+}
+
+func copyBlank(blank []int) []int {
+	return []int{blank[0], blank[1]}
+}
+
+func copyBoard(board [][]int) [][]int {
+	newBoard := make([][]int, 3)
+	newBoard[0] = make([]int, 3)
+	newBoard[1] = make([]int, 3)
+	newBoard[2] = make([]int, 3)
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			newBoard[i][j] = board[i][j]
+		}
+	}
+	return newBoard
 }
 
 func main() {
@@ -37,35 +131,38 @@ func main() {
 
 	init := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 0}}
 	blank := []int{2, 2}
-	print(init)
 	fmt.Println("==========START===========")
-	randomPuzzle(init, blank, 5)
+	//randomPuzzle(init, blank, 5)
 	fmt.Println("===========BFS===========")
-
 	bfs(goal, init, blank)
 }
 
 func move(board [][]int, blank []int, direction string) ([][]int, []int, error) {
 	dir := strings.ToUpper(direction)
+	newBoard := copyBoard(board)
+	newBlank := copyBlank(blank)
 	fmt.Println("Direction : ", dir)
-	if canMove(blank, dir) {
+	print(newBoard)
+	fmt.Println("Blank :", newBlank)
+	if canMove(newBlank, dir) {
 		switch dir {
 		case "U":
-			board, blank = moveU(board, blank)
+			moveU(newBoard, newBlank)
 		case "D":
-			board, blank = moveD(board, blank)
+			moveD(newBoard, newBlank)
 		case "L":
-			board, blank = moveL(board, blank)
+			moveL(newBoard, newBlank)
 		case "R":
-			board, blank = moveR(board, blank)
+			moveR(newBoard, newBlank)
 		}
 	} else {
-		return board, blank, errors.New("Wrong move")
+		return newBoard, newBlank, errors.New("Can't move")
 	}
-	print(board)
-	fmt.Println("Blank:", blank)
+	fmt.Println("New Board")
+	print(newBoard)
+	fmt.Println("New Blank:", newBlank)
 	fmt.Println("=========================")
-	return board, blank, nil
+	return newBoard, newBlank, nil
 }
 
 func canMove(blank []int, direction string) bool {
@@ -159,8 +256,8 @@ func print(board [][]int) {
 	fmt.Println(board[2])
 }
 
-func returnToFont(board [][]int) {
-	fmt.Println("Enter returnToont")
+func returnToFront(board [][]int) {
+	fmt.Println("Enter returnToFront")
 	initpuzzle := []int{board[0][0], board[0][1], board[0][2],
 		board[1][0], board[1][1], board[1][2],
 		board[2][0], board[2][1], board[2][2]}
@@ -180,33 +277,4 @@ func returnToFont(board [][]int) {
 	}
 	jsonres, _ := json.Marshal(res)
 	fmt.Println(string(jsonres))
-}
-
-
-// BFS Call this
-func bfs(goal, board [][]int, blank []int) State { // return []Step
-	var stateQ []State = make([]State, 10)
-	i := 0
-	q := 0
-	for {
-		// Dequeue, check with goal
-		if checkIdentical(stateQ[q].board, goal) {
-			return stateQ[q]
-		}
-		// Move UDLR , Enqueue
-		
-
-	}
-	return false
-}
-
-func moveWrap(board [][]int, blank []int, direction string) State {
-	return move(board, blank, direction)
-}
-
-func bfsMove(s *State, dir string) State,err {
-	board, bank, err := move(s.board, s.bank, dir)
-	s.board = board
-	s.bank = bank
-	return State{board: board, bank: bank, err: err}
 }
